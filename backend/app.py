346903ac7bcfdf8deedd941cda8c9df938128a13
Metellus509 <metellusjunior56@gmail.com>
2025-11-08 19:32:12 -0400

    Refactor app.py to include app context and db creation

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,12 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from models import User
 import os
 
+from app import db, app
+
+with app.app_context():
+    db.create_all()
+
+
 load_dotenv()
 
 app = Flask(__name__)
@@ -141,5 +147,6 @@ def register():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Default to 5000 for local dev
     app.run(host='0.0.0.0', port=port)
+
 
 
